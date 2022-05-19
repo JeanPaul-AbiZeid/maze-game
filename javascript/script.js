4f83413869570c97dd9edf8ef878497441bed2af
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 12:39:55 +0300

    changing onload event

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,19 @@
-function setUpEvents () {
-   
+window.addEventListener("onload", function(){
+    //initializing score
+    var score = 0; 
+    var button = document.getElementsByClassName("boundary");
+    console.log(button);
 
-}
+    function mouse_over(item){
+    item.style.border= "1px red solid";
+    };
 
-window.onload = function() {
-    setUpEvents();
+
+    for (var i = 0; i < button.length; i++) {
+        console.log(button[i]);
+        button[i].addEventListener("mouseover", function(){
+            button[i].style.border= "1px red solid";
+        });
+        
+    };
 }
