2fe8d2007df840eae81d3ec4e62d24c87ffd831f
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 22:33:19 +0300

    organizing the code

--- javascript/script.js
+++ javascript/script.js
@@ -5,12 +5,46 @@ window.addEventListener("load", function(){
     var startBtn = document.getElementById("start");
     var endBtn = document.getElementById("end");
     var textBox = document.getElementById("status");
-    var scoreString = document.getElementsByClassName("example")
-    var leave = document.getElementById("game")
+    var scoreString = document.getElementsByClassName("example");
+    var leave = document.getElementById("game");
+    
 
     function changeText(string, change){
         //changing text
         document.getElementById(string).innerHTML = change;
+    }
+
+    function alertFunction() {
+        alert("Attempt of cheating, you will get -10 score.")
+        score += -10;
+        scoreString[0].innerHTML = "Your Score:" + score; //displaying the score
+    };
+
+    function borderTouch(){
+        for (var j = 0; j < button.length; j++) {
+            button[j].style.background = "#ff8888";
+        }
+        changeText("status", "You Lost, go back to start to score more");
+        toggle = false;
+        score += -10;
+        scoreString[0].innerHTML = "Your Score:" + score; //displaying the score
+    };
+
+    function startAgain (){
+        changeText("status", "Again");
+            for (var j = 0; j < button.length; j++) {
+                button[j].style.background = "#eeeeee";
+            }
+            toggle = true;
+    }
+
+    function finish (){
+        if (toggle == true){
+            changeText("status", "You Won, go back to start to score more");
+            score += 5;
+            scoreString[0].innerHTML = "Your Score:" + score;  //displaying the score
+            toggle = false;  //to increase the score once per path
+            }
     }
 
     startBtn.addEventListener("click", function(){
@@ -18,45 +52,15 @@ window.addEventListener("load", function(){
 
         scoreString[0].innerHTML = "Your Score:" + score; //displaying the score
 
-        leave.addEventListener("mouseleave", function(){    //penelty for leaving the border of th game
-            alert("Attempt of cheating, you will get -10 score.")
-            score += -10;
-            scoreString[0].innerHTML = "Your Score:" + score;
-        })
+        leave.addEventListener("mouseleave", alertFunction);    //penelty for leaving the border of th game
         
-
-        console.log(score);
         for (var i = 0; i < button.length; i++) {   //changing the color on touching the borders
-            button[i].addEventListener("mouseover", function(){
-                for (var j = 0; j < button.length; j++) {
-                    button[j].style.background = "#ff8888";
-                }
-                changeText("status", "You Lost, go back to start to score more");
-                toggle = false;
-                score += -10;
-                scoreString[0].innerHTML = "Your Score:" + score; //displaying the score
-                console.log(score);
-            });
+            button[i].addEventListener("mouseover", borderTouch)
         };
 
-        start.addEventListener("mouseover", function(){    //resetting the color on touching the start button
-            changeText("status", "Again");
-            for (var j = 0; j < button.length; j++) {
-                button[j].style.background = "#eeeeee";
-            }
-            toggle = true;
-            });
+        start.addEventListener("mouseover", startAgain) ;   //resetting the color on touching the start button
 
-        
-        endBtn.addEventListener("mouseover", function(){   //finishing the maze
-            if (toggle == true){
-            changeText("status", "You Won, go back to start to score more");
-            score += 5;
-            scoreString[0].innerHTML = "Your Score:" + score;  //displaying the score
-            toggle = false;  //to increase the score once per path
-            console.log(score);
-            }
-        })
+        endBtn.addEventListener("mouseover", finish)   //finishing the maze  
     })
     
 })
