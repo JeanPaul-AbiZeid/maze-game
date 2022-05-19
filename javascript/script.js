a03d00835e57ba1a6bca8c819a3c9cff5d220bfd
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 19:06:02 +0300

    adding condition to the end button

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,7 @@ window.addEventListener("load", function(){
     var startBtn = document.getElementById("start");
     var endBtn = document.getElementById("end");
     var textBox = document.getElementById("status");
+    var toggle = true;
 
     function changeText(string, change){
         //changing text
@@ -13,13 +14,17 @@ window.addEventListener("load", function(){
 
     startBtn.addEventListener("click", function(){
 
+        console.log(score);
         for (var i = 0; i < button.length; i++) {   //changing the color on touching the borders
             button[i].addEventListener("mouseover", function(){
                 for (var j = 0; j < button.length; j++) {
                     button[j].style.background = "#ff8888";
                 }
                 changeText("status", "You Lost");
+                toggle = false;
+                console.log(toggle)
                 score += -10;
+                console.log(score);
             });
         };
 
@@ -28,12 +33,19 @@ window.addEventListener("load", function(){
             for (var j = 0; j < button.length; j++) {
                 button[j].style.background = "#eeeeee";
             }
-            });       
+            toggle = true;
+            console.log(toggle)
+            });
 
+        
         endBtn.addEventListener("mouseover", function(){   //finishing the maze
+            if (toggle == true){
             changeText("status", "You Won");
+            console.log(toggle)
             score += 5;
-        })
+            console.log(score);
+            }
+        })            
     })
     
 })
