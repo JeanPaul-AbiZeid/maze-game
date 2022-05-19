e4063545e60f7326b3e897dd9d1ad8faabf5bd66
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 15:13:08 +0300

    adding mouse over start for resetting color

--- javascript/script.js
+++ javascript/script.js
@@ -4,21 +4,32 @@ window.addEventListener("load", function(){
     var button = document.getElementsByClassName("boundary");
     var startBtn = document.getElementById("start");
     var endBtn = document.getElementById("end");
+    var textBox = document.getElementById("status");
 
+    function changeText(){
+        //changing text
+    }
     startBtn.addEventListener("click", function(){
         for (var i = 0; i < button.length; i++) {
             button[i].addEventListener("mouseover", function(){
                 for (var j = 0; j < button.length; j++) {
-                    button[j].style.background = "red";
+                    button[j].style.background = "#ff8888";
                 }
+                changeText();
                 score += -10;
             });
         };
+
+        start.addEventListener("mouseover", function(){
+            for (var j = 0; j < button.length; j++) {
+                button[j].style.background = "#eeeeee";
+            }
+            });       
+
         endBtn.addEventListener("mouseover", function(){
+            changeText();
             score += 5;
         })
     })
-    
-
     
 })
