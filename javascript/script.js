427d94fd349269c3647240f38ac70c6f9331a0ac
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 16:26:47 +0300

    adding the text change after loosing or winning

--- javascript/script.js
+++ javascript/script.js
@@ -6,28 +6,32 @@ window.addEventListener("load", function(){
     var endBtn = document.getElementById("end");
     var textBox = document.getElementById("status");
 
-    function changeText(){
+    function changeText(string, change){
         //changing text
+        document.getElementById(string).innerHTML = change;
     }
+
     startBtn.addEventListener("click", function(){
+
         for (var i = 0; i < button.length; i++) {   //changing the color on touching the borders
             button[i].addEventListener("mouseover", function(){
                 for (var j = 0; j < button.length; j++) {
                     button[j].style.background = "#ff8888";
                 }
-                changeText();
+                changeText("status", "You Lost");
                 score += -10;
             });
         };
 
         start.addEventListener("mouseover", function(){    //resetting the color on touching the start button
+            changeText("status", "Again");
             for (var j = 0; j < button.length; j++) {
                 button[j].style.background = "#eeeeee";
             }
             });       
 
         endBtn.addEventListener("mouseover", function(){   //finishing the maze
-            changeText();
+            changeText("status", "You Won");
             score += 5;
         })
     })
