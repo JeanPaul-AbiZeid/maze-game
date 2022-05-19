0e7d922d9868c43ba1b27e8aac606793c0bdbed1
jean paul <jeanpaulabizeid@gmail.com>
2022-05-19 21:08:10 +0300

    applying penalty for leaving the border

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function(){
     var endBtn = document.getElementById("end");
     var textBox = document.getElementById("status");
     var scoreString = document.getElementsByClassName("example")
-    
+    var leave = document.getElementById("game")
 
     function changeText(string, change){
         //changing text
@@ -15,17 +15,26 @@ window.addEventListener("load", function(){
 
     startBtn.addEventListener("click", function(){
         var toggle = true;
-        scoreString[0].innerHTML = "Your Score:" + score;
+
+        scoreString[0].innerHTML = "Your Score:" + score; //displaying the score
+
+        leave.addEventListener("mouseleave", function(){    //penelty for leaving the border of th game
+            alert("Attempt of cheating, you will get -10 score.")
+            score += -10;
+            scoreString[0].innerHTML = "Your Score:" + score;
+        })
+        
+
         console.log(score);
         for (var i = 0; i < button.length; i++) {   //changing the color on touching the borders
             button[i].addEventListener("mouseover", function(){
                 for (var j = 0; j < button.length; j++) {
                     button[j].style.background = "#ff8888";
                 }
-                changeText("status", "You Lost");
+                changeText("status", "You Lost, go back to start to score more");
                 toggle = false;
                 score += -10;
-                scoreString[0].innerHTML = "Your Score:" + score;
+                scoreString[0].innerHTML = "Your Score:" + score; //displaying the score
                 console.log(score);
             });
         };
@@ -41,9 +50,9 @@ window.addEventListener("load", function(){
         
         endBtn.addEventListener("mouseover", function(){   //finishing the maze
             if (toggle == true){
-            changeText("status", "You Won");
+            changeText("status", "You Won, go back to start to score more");
             score += 5;
-            scoreString[0].innerHTML = "Your Score:" + score;
+            scoreString[0].innerHTML = "Your Score:" + score;  //displaying the score
             toggle = false;  //to increase the score once per path
             console.log(score);
             }
